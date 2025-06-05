753f3edbc0c967c7be118528f471e70e39c1c283
TomBos <tombos255@gmail.com>
2025-06-05 16:53:51 +0200

    Updated Master

--- master.py
+++ master.py
@@ -2,6 +2,8 @@ import socket
 import yaml
 import sys
 
+KB = 1024
+
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
@@ -15,11 +17,22 @@ ftp_server = (ftp_host, ftp_port);
 isVerbose = 0
 if '-v' in sys.argv:
     isVerbose = 1
+    print("\nVERBOSE OUTPUT ENABLED \n")
+
+
+def invokeCMD(socketObject, command, isVerbose=0, bufferSize=KB):
+    socketObject.sendall(command.encode())
+    response = socketObject.recv(bufferSize) 
+    if isVerbose:
+        print("Server says: ", response.decode())
+
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(ftp_server)
 if isVerbose:
     print("Connected:", sock)
+    print(" ")
 
 greeting = sock.recv(4096)
 if isVerbose:
@@ -28,17 +41,16 @@ if isVerbose:
 # b => send byte encoded
 # CLRF (\r\n) => Carriage Return + Line Feed => end ftp command
 cmd = f"USER {ftp_user}\r\n"
-sock.sendall(cmd.encode())
-
-response = sock.recv(4096)
-if isVerbose:
-    print("Server says:", response.decode())
+invokeCMD(sock,cmd,isVerbose)
 
 cmd = f"PASS {ftp_pass}\r\n"
-sock.sendall(cmd.encode())
+invokeCMD(sock,cmd,isVerbose)
 
-response = sock.recv(4096)
-print(response.decode())
+cmd = f"PWD \r\n"
+invokeCMD(sock,cmd,isVerbose)
+
+cmd = f"LS \r\n"
+invokeCMD(sock,cmd,isVerbose)
 
 sock.close()
 
