407cc7092480bdaf0475d46741d787b1ce30ebd3
TomBos <tombos255@gmail.com>
2025-06-08 21:18:51 +0200

    Updated the installer

--- install.sh
+++ install.sh
@@ -4,12 +4,18 @@ set -e
 
 INSTALL_DIR="$HOME/.local/share/FTPFS"
 BIN_DIR="$HOME/.local/bin"
+CONFIG_DIR="$HOME/.config/FTPFS"
 WRAPPER="$BIN_DIR/FTPFS"
 
 echo "[*] Installing FTPFS to $INSTALL_DIR..."
 
 mkdir -p "$INSTALL_DIR"
 cp -r ./src/* "$INSTALL_DIR"
+
+echo "[*] Copying example config to $CONFIG_DIR..."
+
+mkdir -p "$CONFIG_DIR"
+cp -r "./src/config.example.yaml" "$CONFIG_DIR/config.yaml"
 
 echo "[*] Creating wrapper script at $WRAPPER..."
 mkdir -p "$BIN_DIR"
