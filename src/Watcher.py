136475bb384f7e072fc51c4393730f914b6c02cf
Tomáš Bosák <85733005+TomBos@users.noreply.github.com>
2025-06-09 13:35:26 +0200

    Update Watcher.py

--- src/Watcher.py
+++ src/Watcher.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import pyinotify
+import threading
+
+modifyDebounceTimers = {}
+createDebounceTimers = {}
 
 class fileWatcher(pyinotify.ProcessEvent):
     def __init__(self, socket, logs, dirMapping, maxBufferSize = 1024):
@@ -11,30 +15,46 @@ class fileWatcher(pyinotify.ProcessEvent):
 
     def process_IN_CREATE(self, event):
         localPath = event.pathname
-        remotePath = self.getRemotePath(localPath) 
-        
-        if not event.dir:
-            self.logs.log(f"Uploading {localPath} to {remotePath}")
-            self.socket.overrideFile(localPath, remotePath, self.logs, self.maximumBufferSize)
-        else:
-            self.logs.log(f"Creating directory on remote: {remotePath}")
-            response = self.socket.createDirectory(remotePath, self.maximumBufferSize) 
-            if response.startswith('257'):
-                self.logs.log(f"Created  : {remotePath}", 1)
+        remotePath = self.getRemotePath(localPath)
+
+        def createEntry():
+            if not event.dir:
+                self.logs.log(f"Uploading {localPath} to {remotePath}")
+                self.socket.overrideFile(localPath, remotePath, self.logs, self.maximumBufferSize)
             else:
-                self.logs.log(f"Failed to create  : {remotePath}",1)
-        
+                self.logs.log(f"Creating directory on remote: {remotePath}")
+                response = self.socket.createDirectory(remotePath, self.maximumBufferSize) 
+                if response.startswith('257'):
+                    self.logs.log(f"Created  : {remotePath}", 1)
+                else:
+                    self.logs.log(f"Failed to create  : {remotePath}",1)
+            createDebounceTimers.pop(localPath, None)
+
+        if localPath in createDebounceTimers:
+            createDebounceTimers[localPath].cancel()
+
+        timer = threading.Timer(0.2, handle_create)
+        createDebounceTimers[localPath] = timer
+        timer.start()        
         return
 
 
     def process_IN_MODIFY(self, event):
         localPath = event.pathname
         remotePath = self.getRemotePath(localPath) 
-        
-        if not event.dir:
-            self.logs.log(f"Uploading {localPath} to {remotePath}")
-            self.socket.overrideFile(localPath, remotePath, self.logs, self.maximumBufferSize)
-        
+
+        def uploadFile():
+            if not event.dir:
+                self.logs.log(f"Uploading {localPath} to {remotePath}")
+                self.socket.overrideFile(localPath, remotePath, self.logs, self.maximumBufferSize)
+            modifyDebounceTimers.pop(localPath, None)
+
+        if localPath in modifyDebounceTimers:
+            modifyDebounceTimers[localPath].cancel()
+
+        timer = threading.Timer(0.2, uploadFile)
+        modifyDebounceTimers[localPath] = timer
+        timer.start()
         return
 
 
