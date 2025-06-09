ebcd58f8f1336d622f533f3c8076bb88babee4ae
Tomáš Bosák <85733005+TomBos@users.noreply.github.com>
2025-06-09 15:46:56 +0200

    Update Watcher.py

--- src/Watcher.py
+++ src/Watcher.py
@@ -22,7 +22,7 @@ class fileWatcher(pyinotify.ProcessEvent):
         now = time.time()
         elapsed = now - lastUpload["timeStamp"]
 
-        if lastUpload["path"] == localPath and elapsed < 0.5:
+        if lastUpload["path"] == localPath and elapsed < 1:
             return
 
         lastUpload["path"] = localPath
@@ -49,7 +49,7 @@ class fileWatcher(pyinotify.ProcessEvent):
         now = time.time()
         elapsed = now - lastUpload["timeStamp"]
 
-        if lastUpload["path"] == localPath and elapsed < 0.5:
+        if lastUpload["path"] == localPath and elapsed < 1:
             return
 
         lastUpload["path"] = localPath
